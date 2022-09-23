fd694b554257a19aee4ff59cb3639d9a8a0d7334
AlexanderKokhno <alexander.kokhno1@gmail.com>
2022-09-22 21:46:20 -0400

    up to #4

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = 'mario';
+var age = 30;
+var isB = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7)); // circ is a function
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+let character = 'mario';
+let age = 30;
+let isB = false;
+
+
+const circ = (diameter:number) => { // circ is a function
+  return diameter * Math.PI;
+}
+
+console.log(circ(7)); // circ is a function
